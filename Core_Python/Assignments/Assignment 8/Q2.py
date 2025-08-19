4ad21509d206da13912d79c5c25e725adeb1bab9
Omkar Shashikant Kulkarni <121794647+omkarkulkarni35@users.noreply.github.com>
2025-08-19 16:21:29 +0530

    Delete Core_Python/Assignments/Assignment 8 directory

--- Core_Python/Assignments/Assignment 8/Q2.py
+++
@@ -1,7 +0,0 @@
-def area_of_circle(radius):
-    pi = 3.14
-    return pi * radius * radius
-
-r = float(input("Enter radius of circle: "))
-area = area_of_circle(r)
-print("Area of circle =", area)
